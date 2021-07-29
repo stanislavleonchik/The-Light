dadbaba6ba4f09ae771b81d86caace7c7d961881
Stanislav Leonchik <leonstas1@mail.ru>
2021-07-29 07:59:03 +0300

    added different colors

--- TheLight/ViewController.swift
+++ TheLight/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 class ViewController: UIViewController {
     
     
-    var isTurnOn = true
+    var isTurnOn = 0
     
     
     override var prefersStatusBarHidden: Bool {
@@ -20,18 +20,41 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         updateUI()
+        isTurnOn += 1
         // Do any additional setup after loading the view.
     }
     
     // MARK: - Methods
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        isTurnOn.toggle()
         updateUI()
+        if isTurnOn == 8 {
+            isTurnOn = 0
+        }
+        isTurnOn += 1
     }
     
     fileprivate func updateUI() {
-        view.backgroundColor = isTurnOn ? .white : .black
+        switch isTurnOn {
+        case 0:
+            view.backgroundColor = .white
+        case 1:
+            view.backgroundColor = .red
+        case 2:
+            view.backgroundColor = .orange
+        case 3:
+            view.backgroundColor = .yellow
+        case 4:
+            view.backgroundColor = .green
+        case 5:
+            view.backgroundColor = .blue
+        case 6:
+            view.backgroundColor = .cyan
+        case 7:
+            view.backgroundColor = .purple
+        default:
+            view.backgroundColor = .white
+        }
     }
 }
 
