1729ca27beecc1c1fd3dc98010863272b4373ed8
Stanislav Leonchik <leonstas1@mail.ru>
2021-08-21 15:09:51 +0300

    Added physical torch

--- TheLight/ViewController.swift
+++ TheLight/ViewController.swift
@@ -4,13 +4,14 @@
 //
 //  Created by Stanislav Leonchik on /297/21.
 //
-
+import AVFoundation
 import UIKit
 
 class ViewController: UIViewController {
     
     
-    var isTurnOn = 0
+    var traficColor = 0
+    var torchIsOn = false
     
     
     override var prefersStatusBarHidden: Bool {
@@ -20,7 +21,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         updateUI()
-        isTurnOn += 1
+        traficColor += 1
         // Do any additional setup after loading the view.
     }
     
@@ -28,22 +29,53 @@ class ViewController: UIViewController {
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         updateUI()
-        if isTurnOn == 3 {
-            isTurnOn = 0
+        if traficColor == 3 {
+            traficColor = 0
         }
-        isTurnOn += 1
+        traficColor += 1
     }
     
+    @IBAction func torchStation(_ sender: Any) {
+        torchIsOn.toggle()
+        switchTorch(isOn: torchIsOn)
+    }
     fileprivate func updateUI() {
-        switch isTurnOn {
+        switch traficColor {
         case 0:
-            view.backgroundColor = .systemRed
+            view.backgroundColor = .red
         case 1:
             view.backgroundColor = .yellow
         case 2:
             view.backgroundColor = .green
         default:
-            view.backgroundColor = .systemRed
+            view.backgroundColor = .red
+        }
+    }
+    
+    fileprivate func switchTorch(isOn: Bool) {
+        guard let device = AVCaptureDevice.default(for: AVMediaType.video) else {
+            return
+        }
+        
+        guard device.hasTorch else {
+            return
+        }
+        do {
+            try device.lockForConfiguration()
+            
+            if (!isOn) {device.torchMode = AVCaptureDevice.TorchMode.off}
+            else {
+                do {
+                    try device.setTorchModeOn(level: 1.0)
+                }
+                catch {
+                    print(error)
+                }
+            }
+            device.unlockForConfiguration()
+        }
+        catch {
+            print(error)
         }
     }
 }
